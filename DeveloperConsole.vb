72b66b2f4a606befb5e8b55a11295d78832a7c6e
horatio nelson <admiralofinternetmeme@outlook.com>
2020-04-05 18:23:21 +0800

    + Console and Patch module

---
+++ DeveloperConsole.vb
@@ -0,0 +1,35 @@
+﻿
+Imports System.Runtime.InteropServices
+Imports BannerLib.Input
+Imports TaleWorlds.InputSystem
+
+Namespace Global.MyVBPatchProject
+    Public Module DeveloperConsole
+        Private m_IsStarted = False
+        Private Declare Sub toggle_imgui_console_visibility Lib "Rgl.dll" Alias "?toggle_imgui_console_visibility@rglCommand_line_manager@@QEAAXXZ" (x As UIntPtr)
+        Public Sub LoadTheHotkeys()
+            If (m_IsStarted) Then Exit Sub
+            m_IsStarted = True
+            Dim hotkey = HotKeys.Create("MyVBPatchProject")
+            Dim key = hotkey.Add("MyVBPatchProject_DeveloperConsole",
+                InputKey.Tilde,
+                HotKeyCategory.Action,
+                "Developer console",
+                "Toggles a developer console"
+            )
+
+            Dim lambda As Func(Of Boolean) =
+            Function()
+                Return True
+            End Function
+            key.WithPredicate(
+                lambda
+            )
+            key.WithOnPressedAction(
+                Sub()
+                    toggle_imgui_console_visibility(New UIntPtr(1))
+                End Sub)
+            hotkey.Build()
+        End Sub
+    End Module
+End Namespace
---
+++ Patch.vb
@@ -0,0 +1,25 @@
+﻿Imports HarmonyLib
+Imports TaleWorlds.CampaignSystem.SandBox.GameComponents.Party
+Imports TaleWorlds.Core
+
+Namespace Global.MyVBPatchProject
+    <HarmonyPatch(GetType(DefaultPartySizeLimitModel), "ShowPartySizeDetail")>
+    Friend Class Patch
+        Public Shared Sub Postfix(strings As List(Of String), ByRef __result As String)
+            InformationManager.ShowInquiry(New InquiryData(
+                "Patch works",
+                "if you see this, the patch works",
+                True,
+                False,
+                "Accept",
+                "",
+                Sub()
+                    'Environment.Exit(1)
+                End Sub,
+                Sub()
+
+                End Sub))
+            __result = "hello world"
+        End Sub
+    End Class
+End Namespace
