6a3036453b35f61e3856bf26239276166e4f44dd
horatio nelson <admiralofinternetmeme@outlook.com>
2020-04-05 12:40:35 +0800

    + My first module.

---
+++ Main.vb
@@ -0,0 +1,29 @@
+﻿Imports TaleWorlds.CampaignSystem
+Imports TaleWorlds.Core
+Imports TaleWorlds.MountAndBlade
+
+Namespace Global.MyVBPatchProject
+    Public Class Main
+        Inherits MBSubModuleBase
+
+        Protected Overrides Sub OnSubModuleLoad()
+            MyBase.OnSubModuleLoad()
+        End Sub
+
+        Protected Overrides Sub OnGameStart(game As Game, gameStarterObject As IGameStarter)
+            Dim campaign = game.GameType
+            If (campaign Is Nothing) Then
+                Debug.WriteLine("oops!")
+                Exit Sub
+            End If
+            Dim campaignStarter = CType(gameStarterObject, CampaignGameStarter)
+            AddBehaviour(campaignStarter)
+        End Sub
+
+        Private Sub AddBehaviour(gameInit As CampaignGameStarter)
+            'gameInit.AddBehavior(New SimpleDayCounter)
+        End Sub
+
+    End Class
+
+End Namespace
